e2178d427a199edfa0edf95570072835322971f6
asnewman <ashznewman@gmail.com>
2021-08-06 11:10:39 -1000

    Clean up api response

--- api/rally.ts
+++ api/rally.ts
@@ -6,11 +6,23 @@ import {client} from "../bot";
 const router = express.Router();
 
 router.get('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const rally = await Rally.findOne({ _id: id });
-  const user  = await client.users.fetch(rally.authorId)
-  console.log(user);
-  res.send(rally);
+  const {id} = req.params;
+  const rally = await Rally.findOne({_id: id});
+  const author = await client.users.fetch(rally.authorId)
+  const usersPromises = rally.userIds.map(userId => client.users.fetch(userId))
+  const backupUsersPromises = rally.backupUserIds.map(backupUserId => client.users.fetch(backupUserId))
+  const users = await Promise.all(usersPromises);
+  const backupUsers = await Promise.all(backupUsersPromises);
+  res.send({
+    id: rally._id,
+    messageId: rally.messageId,
+    gameName: rally.gameName,
+    userCount: rally.userCount,
+    hasFilled: rally.hasFilled,
+    author,
+    users,
+    backupUsers
+  });
 })
 
 export default router;
